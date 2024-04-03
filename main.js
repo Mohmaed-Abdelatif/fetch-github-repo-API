1a368b97f8da433787ea11a537699bd9fc6daaed
mohamed <imohamed.abdelatif@gmail.com>
2024-04-03 13:36:20 +0200

    Upload

--- main.js
+++ main.js
@@ -2,37 +2,47 @@ let myInput = document.querySelector(".get-repos input");
 let myButton = document.querySelector(".get-repos button");
 let reposData = document.querySelector(".show-data");
 
-myButton.onclick = function(){
-    fetchRepos();
-};
-function fetchRepos(){
-    if(myInput.value==""){
-        reposData.innerHTML="<span>Please Write GitHub UserName.<span/>";
-    }else{
-        fetch(`https://api.github.com/users/${myInput.value}/repos`)
-        .then((res)=>{
-            return res.json();
-        })
-        .then((data)=>{
-        reposData.innerHTML=""
-        let i=1;
-        data.forEach(repo => {
-            let mainDiv = document.createElement('div');
-            mainDiv.className = 'repoBox';
-            mainDiv.innerHTML = `
+function fetchRepos() {
+  if (myInput.value == "") {
+    reposData.innerHTML = "<span>Please Write GitHub UserName.<span/>";
+  } else {
+    fetch(`https://api.github.com/users/${myInput.value}/repos`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        reposData.innerHTML = "";
+        let i = 1;
+        data.forEach((repo) => {
+          let mainDiv = document.createElement("div");
+          mainDiv.className = "repoBox";
+          mainDiv.innerHTML = `
+            <div class="name-container">
                 ${i}- ${repo.name}
-                <div>
-                    <a href="https://github.com/${myInput.value}/${repo.name}" target="_blank">Visit<a/>
-                    <span>${repo.stargazers_count}Star<span/>
-                <div/>
+            </div>
                 
+            <div class="data-container">
+                <div >
+                    <a class="link" href="https://github.com/${myInput.value}/${repo.name}" target="_blank"> Visit </a>
+                </div>
+                <div class="star-count">${repo.stargazers_count}Star</div>
+            </div>
             `;
-            reposData.appendChild(mainDiv);
-            i++;
+          reposData.appendChild(mainDiv);
+          i++;
         });
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-    }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
+myButton.onclick = function () {
+  fetchRepos();
 };
+myInput.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    fetchRepos();
+  }
+});
